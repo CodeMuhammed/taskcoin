29c5336186d6cf3945e214b29ef3f895fc9db252
codemuhammed <codemuhammed@gmail.com>
2016-07-04 02:35:24 +0100

    Signed-off-by: codemuhammed <codemuhammed@gmail.com>

--- app_server/models/dbResource.js
+++ app_server/models/dbResource.js
@@ -9,23 +9,23 @@ var url = '';
 
 //import the language driver
 module.exports = function(dbName , app){
-   
-	//This functions accesses the database and creates a pool of opened 
+
+	//This functions accesses the database and creates a pool of opened
 	//connections to the required collections needed by the app
 	var initColls = function (collsArr , cb) {
 		if(!isDBOpened()){
 			MongoClient.connect(url , function(err , db){
 				if(err){
 					throw new Error('DB connection error ');
-				} else { 
+				} else {
 					assert.equal(null ,err);
 					console.log('Connected correcctly to the database');
-					
+
 					//
 					for(var i=0; i<collsArr.length; i++){
 						openedColls[collsArr[i]] = db.collection(collsArr[i]);
 					}
-					
+
 					DBOpened = true;
 					return cb();
 				}
@@ -33,9 +33,9 @@ module.exports = function(dbName , app){
 		} else {
 			return cb();
 		}
-		
+
 	};
-	
+
     //This function returns the valid collection to the client module
 	var model = function(coll){
 		if(!openedColls[coll]){
@@ -43,22 +43,21 @@ module.exports = function(dbName , app){
 		}
 		return openedColls[coll];
 	};
-	
+
 	//
 	var isDBOpened = function(){
 		return DBOpened;
 	}
-	
+
 	//Set db connection string based on the current environment being worked in...
 	if(app.get('env') ==='development'){
        url = 'mongodb://127.0.0.1:27017/taskcoin';
 	} else {
-       url = 'mongodb://'+ process.env.dbuser+ ':'+process.env.dbpassword+'@ds051738.mongolab.com:51738/'+dbName.trim();
+       url = 'mongodb://'+ process.env.dbuser+ ':'+process.env.dbpassword+'@ds011725.mlab.com:11725/taskcoin';
 	}
-	
+
 	return {
 		initColls : initColls,
 		model : model
 	};
 };
-
